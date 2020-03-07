89f7c314e0e9501b978057a58cedd14e0eb45641
Anmei Liu <anmliu@ucdavis.edu>
2020-03-06 18:06:25 -0800

    add nominatim_osr

--- geocode.R
+++ geocode.R
@@ -2,6 +2,19 @@ library(tidyverse)
 library(jsonlite)
 library(dplyr)
 
+nominatim_osm <- function(address = NULL)
+{
+  if(suppressWarnings(is.null(address)))
+    return(data.frame())
+  tryCatch(
+    d <- jsonlite::fromJSON( 
+      gsub('\\@addr\\@', gsub('\\s+', '\\%20', address), 
+           'http://nominatim.openstreetmap.org/search/@addr@?format=json&addressdetails=0&limit=1')
+    ), error = function(c) return(data.frame())
+  )
+  if(length(d) == 0) return(data.frame())
+  return(data.frame(lon = as.numeric(d$lon), lat = as.numeric(d$lat)))
+}
 
 data <- fromJSON("https://data.cms.gov/resource/97k6-zzx3.json")
 
