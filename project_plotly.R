efe694bbae84d84ccb125d9baca5dbb3494d6ac0
Alanna Gerton <amgerton@ucdavis.edu>
2020-03-08 12:54:56 -0700

    data

--- project_plotly.R
+++ project_plotly.R
@@ -1,3 +1,12 @@
 ##############################################
 # R SCRIPT FOR DATA PLOTLY DATA VISUALIZATIONS
 ##############################################
+
+library(tidyverse)
+library(jsonlite)
+library(dplyr)
+
+data <- fromJSON("https://data.cms.gov/resource/97k6-zzx3.json")
+
+data <- data %>% 
+  mutate(whole_address = paste(provider_street_address, provider_city, provider_state, sep = ', '))
