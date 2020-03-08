049d5ce0e67e37cade5d6f70cd4b6645a8965b8d
karateJackson <itsearl100@gmail.com>
2020-03-08 13:19:40 -0700

    NEW CODE

--- project_plotly.R
+++ project_plotly.R
@@ -2,11 +2,36 @@
 # R SCRIPT FOR DATA PLOTLY DATA VISUALIZATIONS
 ##############################################
 
-library(tidyverse)
+library(httr)
 library(jsonlite)
-library(dplyr)
+library(tidyverse)
+library(plotly)
 
-data <- fromJSON("https://data.cms.gov/resource/97k6-zzx3.json")
+########################################################
+# https://data.cms.gov/Medicare-Inpatient/Inpatient-Prospective-Payment-System-IPPS-Provider/97k6-zzx3
+# ^^^ REFER TO THIS WEBSITE FOR VARIABLE DESCRIPTION ###
 
-data <- data %>% 
-  mutate(whole_address = paste(provider_street_address, provider_city, provider_state, sep = ', '))
+# Get data from the following source in JSON: 
+# Inpatient Prospective Payment System (IPPS) Provider Summary for the 
+# Top 100 Diagnosis-Related Groups (DRG) - FY2011
+medicareIPPS.data <- fromJSON("https://data.cms.gov/resource/97k6-zzx3.json")
+View(medicareIPPS.data)
+
+# look at the data after removing duplicate rows
+View(medicareIPPS.data %>%
+       distinct())
+
+
+
+# Check unique provider names b/c of duplicate DRG codes
+unique(medicareIPPS.data$provider_id) # 1000 unique provider
+
+# Look at unique number of providers for each state
+provider.state <- medicareIPPS.data %>%
+  group_by(provider_state) %>%
+  summarize(number_providers_per_state = n_distinct(provider_id)) %>%
+  arrange(desc(number_providers_per_state))
+
+
+# **********************************
+# ***** PLAY W/ PLOTLY HERE! ***** #
