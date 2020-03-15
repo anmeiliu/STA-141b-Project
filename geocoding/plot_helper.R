b3db547b5df28b114d6067104cb00c57b769624c
Anmei Liu <anmliu@ucdavis.edu>
2020-03-15 16:10:33 -0700

    cosmetic modification to number of digits to display

--- geocoding/plot_helper.R
+++ geocoding/plot_helper.R
@@ -2,11 +2,11 @@ pretty_print_large_number <- function(num) {
   string <- ifelse(num < 1000, 
          format(num, digits = 3),
          ifelse(num < 1000000, 
-                paste(format(num/1000, trim = TRUE, digits = 3), "k", sep = ""),
+                paste(format(num/1000, trim = TRUE, digits = 2), "k", sep = ""),
                 ifelse(num < 1000000000, 
-                       paste(format(num/1000000, trim = TRUE, digits = 3), "m", sep = ""),
+                       paste(format(num/1000000, trim = TRUE, digits = 2), "m", sep = ""),
                        ifelse(num < 1000000000000,
-                              paste(format(num/1000000000, trim = TRUE, digits = 3), "b", sep = ""),
+                              paste(format(num/1000000000, trim = TRUE, digits = 2), "b", sep = ""),
                               NA
                        )
                 )
