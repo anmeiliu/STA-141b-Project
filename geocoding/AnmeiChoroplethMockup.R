bd45a2abab788e89a6a4747a297c1e95176d9a0a
Anmei Liu <anmliu@ucdavis.edu>
2020-03-16 11:26:55 -0700

    comment and code formatting cleanup

--- geocoding/AnmeiChoroplethMockup.R
+++ geocoding/AnmeiChoroplethMockup.R
@@ -7,88 +7,110 @@ library(RSocrata)
 
 source("plot_helper.R")
 
+# define constants/settings ====
+log_scale_base <- 2 # what log base to log scale data by
+tick_intervals <-
+  c(0, 0.2, 0.4, 0.6, 0.8, 1) # where to place ticks on the scale
+default_tick_mode <-
+  "auto" # default tick mode when it doesn't need to be overridden
 
-# define constants/settings
-log_scale_base <- 2
-tick_intervals <- c(0, 0.2, 0.4, 0.6, 0.8, 1)
-default_tick_mode <- "auto"
+# preloads ====
+# not sure what the source for this
+zipcode_ref <-
+  read_csv("uszips.csv",
+           col_types = cols(zip = col_integer(),
+                            county_fips = col_character()))
 
-# preloads
-zipcode_ref <- read_csv("uszips.csv", col_types = cols(zip = col_integer(), 
-                                                       county_fips = col_character()))
-county_pop_call <- GET("https://api.census.gov/data/2018/pep/population",
-                query = list(
-                  get = "POP",
-                  `for` = "county:*",
-                  `in` = "state:*"
-                ))
-county_mat <- data.frame(matrix(unlist(content(county_pop_call)), ncol = 3, byrow = TRUE)[-1,])
-county_ref <- county_mat %>% 
+# 2018 population data from census (2019 API not available?)
+county_pop_call <-
+  GET(
+    "https://api.census.gov/data/2018/pep/population",
+    query = list(get = "POP",
+                 `for` = "county:*",
+                 `in` = "state:*")
+  )
+county_mat <-
+  data.frame(matrix(unlist(content(county_pop_call)), ncol = 3, byrow = TRUE)[-1, ])
+county_ref <- county_mat %>%
   unite(county_fips, c(X2, X3), sep = "") %>%
-  mutate(pop =  strtoi(X1)) %>% 
+  mutate(pop =  strtoi(X1)) %>%
   select(-X1)
 
-drg_groups <- get_api_call(list("$select" = "distinct drg_definition"))
+# diagnosis-related group list for selector
+drg_groups <-
+  get_api_call(list("$select" = "distinct drg_definition"))
 
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-  
-  sidebarLayout(
-    sidebarPanel(
-      radioButtons("aggregation_level", 
-                   "Aggregation level:", 
-                   c("State" = "st", "County (slower)" = "cty"),
-                   selected = "st"
-                   ),
-      checkboxInput("log_scale",
-                    "Log scale the data?"),
-      checkboxInput("correct_by_pop",
-                    "Correct for population?"),
-      selectInput("drg_group", 
-                  "Filter by DRG group",
-                  c("All groups", drg_groups$drg_definition))
+# user interface ====
+ui <- fluidPage(sidebarLayout(
+  sidebarPanel(
+    radioButtons(
+      "aggregation_level",
+      "Aggregation level:",
+      c("State" = "st", "County (slower)" = "cty"),
+      selected = "st"
     ),
-    mainPanel(
-      plotlyOutput("choropleth_plot")
+    checkboxInput("log_scale",
+                  "Log scale the data?"),
+    checkboxInput("correct_by_pop",
+                  "Correct for population?"),
+    selectInput(
+      "drg_group",
+      "Filter by DRG group",
+      c("All groups", drg_groups$drg_definition)
     )
-  )
-)
+  ),
+  mainPanel(plotlyOutput("choropleth_plot"))
+))
 
+# server function ====
 server <- function(input, output) {
-  
-  agg_by_county <- reactive({    
+  # reactive wrapper for aggregation level (which is used in several places)
+  agg_by_county <- reactive({
     input$aggregation_level == "cty"
-    })
+  })
   
+  # reactive generation for API call parameters
   api_call_param <- reactive({
     if (agg_by_county()) {
-      api_call_param <- list("$select" = c("provider_zip_code","sum(total_discharges)"), 
-                             "$group" = "provider_zip_code", "$limit" = "50000")
+      # limit forces all data to be displayed
+      api_call_param <-
+        list(
+          "$select" = c("provider_zip_code", "sum(total_discharges)"),
+          "$group" = "provider_zip_code",
+          "$limit" = "50000"
+        )
     } else {
-      api_call_param <- list("$select" = c("provider_state","sum(total_discharges)"), 
-                             "$group" = "provider_state")
+      api_call_param <-
+        list(
+          "$select" = c("provider_state", "sum(total_discharges)"),
+          "$group" = "provider_state"
+        )
     }
     
+    # filter by DRG if option is selected
+    # all DRGs contain spaces so must be passed as string literals
+    # some DRGs contain ampersands so must be URL encoded
     if (input$drg_group != "All groups") {
-      api_call_param[["$where"]] <- paste("drg_definition='", 
-                                          URLencode(input$drg_group, reserved = TRUE), 
-                                          "'", sep = "")
+      api_call_param[["$where"]] <- paste("drg_definition='",
+                                          URLencode(input$drg_group, reserved = TRUE),
+                                          "'",
+                                          sep = "")
     }
     api_call_param
   })
   
+  # creates the inital aggregate dataframe
   construct_aggregate <- reactive({
     if (agg_by_county()) {
       zip_agg <- get_api_call(api_call_param())
       
-      agg <- zip_agg %>% 
-        mutate(zip = as.integer(provider_zip_code)) %>% 
-        left_join(zipcode_ref, by = "zip") %>% 
-        select(sum_total_discharges, 
+      agg <- zip_agg %>%
+        mutate(zip = as.integer(provider_zip_code)) %>%
+        left_join(zipcode_ref, by = "zip") %>%
+        select(sum_total_discharges,
                county_fips,
-               county_name) %>% 
-        group_by(county_fips, county_name) %>% 
+               county_name) %>%
+        group_by(county_fips, county_name) %>%
         summarise(value = sum(as.integer(sum_total_discharges))) %>%
         left_join(county_ref, by = "county_fips") %>%
         drop_na() %>%
@@ -99,17 +121,20 @@ server <- function(input, output) {
       
     } else {
       state_agg <- get_api_call(api_call_param())
-      agg <- state_agg %>% 
-        rename(state_id = provider_state) %>% 
-        left_join(zipcode_ref %>% 
-                    group_by(state_id, state_name) %>% 
-                    summarize(population = sum(population)), 
-                  by = "state_id") %>% 
-        select(sum_total_discharges, 
+      agg <- state_agg %>%
+        rename(state_id = provider_state) %>%
+        left_join(
+          zipcode_ref %>%
+            group_by(state_id, state_name) %>%
+            summarize(population = sum(population)),
+          by = "state_id"
+        ) %>%
+        select(sum_total_discharges,
                population,
-               state_name) %>% 
-        group_by(state_name) %>% 
-        summarise(value = sum(as.integer(sum_total_discharges)), pop = sum(population)) %>%
+               state_name) %>%
+        group_by(state_name) %>%
+        summarise(value = sum(as.integer(sum_total_discharges)),
+                  pop = sum(population)) %>%
         mutate(region_name = state_name) %>%
         rename(region = state_name)
       
@@ -117,82 +142,87 @@ server <- function(input, output) {
     }
   })
   
+  # plot the choropleth
   output$choropleth_plot <- renderPlotly({
-    
+    # set aggregation-level specific display options
+    # plotly's mapbox choropleth prefers jsons as URLs
     if (agg_by_county()) {
-      agg_geom <- "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"
+      agg_geom <-
+        "https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json"
       agg_key <- "id"
       colorscale <- "Reds"
-      
     } else {
-      agg_geom <- "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json"
+      agg_geom <-
+        "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json"
       agg_key <- "properties.name"
       colorscale <- "Reds"
     }
     
-    agg <- construct_aggregate() %>% mutate(hover = paste(
-      region_name,
-      "<br />Population:",
-      pretty_print_large_number(pop),
-      "<br />Discharges:",
-      pretty_print_large_number(value)
-    ))
+    # add tooltips
+    agg <- construct_aggregate() %>% mutate(
+      hover = paste(
+        region_name,
+        "<br />Population:",
+        pretty_print_large_number(pop),
+        "<br />Discharges:",
+        pretty_print_large_number(value)
+      )
+    )
     
+    # correct values by population if selected
     if (input$correct_by_pop) {
-      agg <- agg %>% mutate(value = value/pop)
+      agg <- agg %>% mutate(value = value / pop)
     }
     
+    # find current minimum and maximum of values
     zmin <- min(agg$value)
     zmax <- max(agg$value)
     
+    # rescale values by log if necessary
     if (input$log_scale) {
       agg <- agg %>% mutate(value = log(value, log_scale_base))
       zmin <- log(zmin, log_scale_base)
       zmax <- log(zmax, log_scale_base)
       tickvals <- zmin + ((zmax - zmin) * tick_intervals)
-      ticktext <- pretty_print_large_number(log_scale_base^tickvals)
-      tickmode <- "array"
+      ticktext <- pretty_print_large_number(log_scale_base ^ tickvals)
+      tickmode <- "array" # forces choropleth to use passed in tickvals/ticktext
     } else {
       zmin <- 0
       tickvals <- zmin + ((zmax - zmin) * tick_intervals)
       ticktext <- pretty_print_large_number(tickvals)
-      tickmode <- default_tick_mode
+      tickmode <- default_tick_mode # if default is auto, tickvals/ticktext will be ignored
     }
     
-    fig <- plot_ly() 
+    # actual plot generation
+    fig <- plot_ly()
     fig <- fig %>% add_trace(
-      type="choroplethmapbox",
-      geojson=agg_geom,
-      featureidkey=agg_key,
-      locations=agg$region,
-      z=agg$value,
-      colorscale=colorscale,
-      zmin=zmin,
-      zmax=zmax,
-      marker=list(line=list(
-        width=0),
-        opacity=0.5
-      ),
-      text=agg$hover,
+      type = "choroplethmapbox",
+      geojson = agg_geom,
+      featureidkey = agg_key,
+      locations = agg$region,
+      z = agg$value,
+      colorscale = colorscale,
+      zmin = zmin,
+      zmax = zmax,
+      marker = list(line = list(width = 0),
+                    opacity = 0.5),
+      text = agg$hover,
       hoverinfo = "text",
-      colorbar=list(
-        tickmode=tickmode,
-        tickvals=tickvals,
-        ticktext=ticktext
+      colorbar = list(
+        tickmode = tickmode,
+        tickvals = tickvals,
+        ticktext = ticktext
       )
     )
     
-    fig <- fig %>% layout(
-      mapbox=list(
-        style="carto-positron",
-        zoom =2,
-        center=list(lon= -95.71, lat=37.09)
-        )
-      )
+    fig <- fig %>% layout(mapbox = list(
+      style = "carto-positron",
+      zoom = 2,
+      center = list(lon = -95.71, lat = 37.09)
+    ))
     
     fig
   })
 }
 
 shinyApp(ui = ui, server = server)
-
